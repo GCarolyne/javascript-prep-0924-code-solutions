28b99e0e78104cfdabdf93240b66a203dee29c72
Carolyne Golias <goliascarolyne@gmail.com>
2024-10-03 22:41:00 +0000

    Completed javascript-operators-and-expressions.

---
+++ prep-javascript-operators-and-expressions/main.js
@@ -0,0 +1,38 @@
+const number1 = 1;
+const number2 = 2;
+const product = number1 * number2;
+console.log(product);
+console.log(typeof product);
+
+const charge = 100;
+const payment = 25;
+const amountRemaining = charge - payment;
+console.log(amountRemaining);
+console.log(typeof amountRemaining);
+
+const tests = 555;
+const assignments = 200;
+const final = 100;
+const grade = (tests + assignments + final) / 3;
+console.log(grade);
+console.log(typeof grade);
+
+const firstName = 'Carolyne';
+const lastName = 'Golias';
+const fullName = 'Carolyne' + 'Golias';
+console.log(fullName);
+console.log(typeof fullName);
+
+const pH = 3;
+const isAcidic = 7 > pH;
+console.log(isAcidic);
+console.log(typeof isAcidic);
+
+const numberOfSoldiers = 1500;
+const isSparta = numberOfSoldiers === 300;
+console.log(isSparta);
+console.log(typeof isSparta);
+
+let nickname = 'Carolyne Golias' + ' is super';
+console.log(nickname);
+console.log(typeof nickname);
