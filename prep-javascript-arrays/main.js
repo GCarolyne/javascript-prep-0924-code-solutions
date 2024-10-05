9c48fd4d1d4ba173015fda1a81728b4aa67d89f8
Carolyne Golias <goliascarolyne@gmail.com>
2024-10-05 19:33:20 +0000

    Completed prep-javascript-arrays.

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,14 @@
+const languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
+console.log(languages);
+
+const firstElement = 'html';
+console.log('the first element of the array is:', firstElement);
+
+const thirdElement = 'javascript';
+console.log('the third element of the array is:', thirdElement);
+
+const length = languages.length;
+console.log('The length of the array is', languages.length);
+
+const lastElement = languages.length;
+console.log('The last element of the array is', lastElement - 1);
